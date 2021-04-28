3889f79fae55c9e1ce499368560e0f1922a3a792
Tavleen Bhatia <tavleenbhatia@berkeley.edu>
2021-04-28 06:26:46 +0530

    Main landing page

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+
+function redButtonHovered() {
+    document.body.style.backgroundColor = "#8c0000";
+}
+
+document.getElementById("btn1-red").onmouseover = redButtonHovered;
+
+function blueButtonHovered() {
+    document.body.style.backgroundColor = "#693c72";
+}
+
+document.getElementById("btn2-blue").onmouseover = blueButtonHovered;
+
+function greenButtonHovered() {
+    document.body.style.backgroundColor = "#289672";
+}
+document.getElementById("btn3-green").onmouseover = greenButtonHovered;
+
+function yellowButtonHovered() {
+    document.body.style.backgroundColor = "#ef8d32";
+}
+document.getElementById("btn4-yellow").onmouseover = yellowButtonHovered;
